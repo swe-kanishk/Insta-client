52a129f33250e2a9e9ecbb7451bfea7b9451a928
kanishk tiwari <kanishk.swe@gmail.com>
2024-10-10 22:22:46 +0530

    final url changes

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
 
   useEffect(() => {
     if (user) {
-      const socketio = io("http://localhost:8000", {
+      const socketio = io("https://insta-backend-flws.onrender.com", {
         query: {
           userId: user?._id,
         },
